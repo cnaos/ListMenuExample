b934afec29ab9eea5751405306116b6d574455f7
cnaos <cnaos.gm@gmail.com>
2019-11-29 18:29:03 +0900

    databindingを有効にしてListViewを追加。

--- app/src/main/java/io/github/cnaos/example/listmenuexample/MainActivity.kt
+++ app/src/main/java/io/github/cnaos/example/listmenuexample/MainActivity.kt
@@ -1,12 +1,24 @@
 package io.github.cnaos.example.listmenuexample
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
+import com.wada811.databinding.viewBinding
+import io.github.cnaos.example.listmenuexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    val binding by viewBinding { ActivityMainBinding.inflate(it) }
+
+    val listItems = listOf(
+        MyMenuItem("Menu A"),
+        MyMenuItem("Menu B")
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val adapter = ArrayAdapter<MyMenuItem>(this, android.R.layout.simple_list_item_1)
+        binding.listView.adapter = adapter
     }
 }
